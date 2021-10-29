f02d10282547b5e238edc1dbbcd1c5b65f5a0c65
milafreckled <miladul2014@gmail.com>
2021-10-29 09:55:48 +0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';import Layout from '../componenets/layout'
+const AboutPage = () => {
+    return (
+      <Layout pageTitle="About Me">
+
+        <article>Hi there! My name is Mila, and I am a proud creator of this blog!</article>
+        </Layout>
+    )
+  }
+  
+  export default AboutPage
