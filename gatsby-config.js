58d5667135d3995c47bfdc13cbe8e98a5e1a1787
milafreckled <miladul2014@gmail.com>
2023-09-28 11:06:40 +0200

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    // `gatsby-plugin-netlify`
-    //
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
